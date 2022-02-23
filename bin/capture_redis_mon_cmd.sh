abebbe04ad3e12ed8521546b2e1266e71d1fe04e
viragtripathi <15679776+viragtripathi@users.noreply.github.com>
2022-02-23 10:53:37 -0500

    Updated project

---
+++ bin/capture_redis_mon_cmd.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+host="$1"
+port="$2"
+user="$3"
+pass="$4"
+[[ -z "$host" ]] && { echo "Error: Missing redis host e.g. 127.0.0.1"; exit 1; }
+[[ -z "$port" ]] && { echo "Error: Missing redis port e.g. 14000"; exit 1; }
+
+redis-cli -h ${host} -p ${port} --user ${user-default} --pass ${pass-default} monitor | while read line; do echo $line | egrep -v "COMMAND|AUTH|monitor|OK|HELLO|PING|EXISTS|HGETALL|SCAN|TYPE|INFO|flushdb|pos|row|event|snapshot|last_snapshot_record|lsn|ts_usec|lsn_proc|txId" | awk '{for(i=4;i<=NF;i++){printf "%s ", $i}; printf "\n"}' >> redis-${port}_$(date +"%m%d%Y").log; done
+
